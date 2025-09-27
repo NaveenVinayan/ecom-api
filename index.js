00177489006d9766e3d1a0f491a44ff0d906093c
NaveenVinayan <vinayannaveen@gmail.com>
2025-09-27 11:50:29 +0530

    CORB block fix

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.js";
-import productRouter from "./routes/product.js"; 
-import userRouter from "./routes/user.js"; 
-import orderRouter from "./routes/order.js"; 
-import wishlistRouter from "./routes/wishlist.js"; 
+import productRouter from "./routes/product.js";
+import userRouter from "./routes/user.js";
+import orderRouter from "./routes/order.js";
+import wishlistRouter from "./routes/wishlist.js";
 import connectToDatabase from "./db/db.js";
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 connectToDatabase();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 // Define your allowed origins as an array
 const allowedOrigins = [
   "https://ecom-frontend-gules.vercel.app", // Vercel frontend
-  "http://localhost:5173" // Port for local development 
+  "http://localhost:5173", // Port for local development
 ];
 
 // Configure CORS
@@ -22,11 +27,11 @@ const corsOptions = {
   origin: function (origin, callback) {
     // Bypass the origin check if the request has no origin (e.g., Postman)
     if (!origin) return callback(null, true);
-    
+
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
@@ -37,9 +42,8 @@ const corsOptions = {
 // Use the configured CORS middleware
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
-app.use(express.static("public/uploads"));
+app.use(express.static(path.join(__dirname, "public/uploads")));
 
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
