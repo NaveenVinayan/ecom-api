4ef1c187164bfa51df172f1e454e1f7e75c98d82
NaveenVinayan <vinayannaveen@gmail.com>
2025-09-29 11:15:32 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Define your allowed origins as an array
 const allowedOrigins = [
   "https://ecom-frontend-gules.vercel.app", // Vercel frontend
   "http://localhost:5173", // Port for local development
@@ -25,7 +24,6 @@ const allowedOrigins = [
 // Configure CORS
 const corsOptions = {
   origin: function (origin, callback) {
-    // Bypass the origin check if the request has no origin (e.g., Postman)
     if (!origin) return callback(null, true);
 
     if (allowedOrigins.indexOf(origin) !== -1) {
@@ -39,11 +37,15 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 
-// Use the configured CORS middleware
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public/uploads")));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  next();
+}, express.static(path.join(__dirname, "public/uploads")));
 
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
